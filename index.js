a64f96af36bc2cbaf9c63ace9a0b44136af0adb5
emmanueljucaban <emmanueljucaban@gmail.com>
2019-10-16 13:50:46 -0700

    Configure database connection and middleware

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const PORT = process.env.NODE_ENV || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
 
 app.listen(PORT, () => console.log('Port started on port: ' + PORT));
