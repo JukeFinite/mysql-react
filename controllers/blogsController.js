18afa2c3c16eafb01cb7d8263eb8f2b87e6b22c2
emmanueljucaban <emmanueljucaban@gmail.com>
2019-10-17 10:51:00 -0700

    Push controllers

---
+++ controllers/blogsController.js
@@ -0,0 +1,23 @@
+const connection = require('./../config/connection');
+
+module.exports = {
+    getBlogs: (req, res) => {
+        const query = `SELECT * FROM blogs;`;
+        connection.query(query, (err, blogs) => {
+            if(err) {
+                return res.status(404).send(err);
+            }
+            res.json(blogs);
+        });
+    },
+    createBlog: (req, res) => {
+        const { blog } = req.body;
+        const query = `INSERT INTO blogs (blog) VALUES(?)`;
+        connection.query(query, blog, (err, response) => {
+            if(err) {
+                return res.status(403).send(err);
+            }
+            res.send(response); 
+        });
+    }
+};
