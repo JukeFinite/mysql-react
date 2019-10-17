d939c3e9bf37a8e2ca0cf24ca00b980bfcb2bdac
emmanueljucaban <emmanueljucaban@gmail.com>
2019-10-17 10:48:31 -0700

    Create controllers

--- routes/apiRoutes/blogRoutes/index.js
+++ routes/apiRoutes/blogRoutes/index.js
@@ -1,27 +1,14 @@
 const router = require('express').Router();
-const connection = require('./../../../config/connection');
+const blogController = require('./../../../controllers/blogsController');
 
 
-// /api/blog
-router.get('/', (req, res) => {
-    const query = `SELECT * FROM blogs;`;
-    connection.query(query, (err, blogs) => {
-        if(err) {
-            return res.status(404).send(err);
-        }
-        res.json(blogs);
-    });
-});
 
-router.post('/', (req, res) => {
-    const { blog } = req.body;
-    const query = `INSERT INTO blogs (blog) VALUES(?)`;
-    connection.query(query, blog, (err, response) => {
-        if(err) {
-            return res.status(403).send(err);
-        }
-        res.send(response); 
-    });
-});
+// /api/blogs
+router.route('/')
+    .get(blogController.getBlogs)
+    .post(blogController.createBlog)
+
+
+
 
 module.exports = router;
