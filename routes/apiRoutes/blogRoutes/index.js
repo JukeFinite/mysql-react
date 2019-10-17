944b5d8fe95f0d3018cc544f11621c45a02d72a1
emmanueljucaban <emmanueljucaban@gmail.com>
2019-10-17 10:01:16 -0700

    Create routes for blogs and comments

--- routes/apiRoutes/blogRoutes/index.js
+++ routes/apiRoutes/blogRoutes/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
-
+const connection = require('./../../../config/connection');
 // /api/blog
 
 router.get('/', (req, res) => {
-    console.log(`I'm hit!`);
+    const query = `SELECT * FROM blogs;`;
+    connection.query(query, (err, blogs) => {
+        if(err) {
+            return res.status(404).send(err);
+        }
+        res.json(blogs);
+    });
 });
 
 module.exports = router;
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,7 +4,7 @@ const blogRoutes = require('./blogRoutes');
 const commentRoutes = require('./commentRoutes');
 // /api  prepended to every route declared in here
 // /api/blog
-router.use('/blog', blogRoutes);
+router.use('/blogs', blogRoutes);
 
 // /api/comment
 router.use('/comments', commentRoutes);
